dac20fa30e5a1b2ac98238354c63ae8b61090814
Oxfy <17604591+Oxfy@users.noreply.github.com>
2024-08-22 23:22:21 +0200

    Add password generation function

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
     "Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+",
      "=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+let passwordOne = document.getElementById("word_one")
+let passwordTwo = document.getElementById("word_two")
+
+function generatePasswords(){
+    passwordOne.textContent = ""
+    passwordTwo.textContent = ""
+    for(let i = 0; i < 15; i++){  
+
+        passwordOne.textContent += characters[Math.floor(Math.random() * characters.length)]
+    }
+    for(let i = 0; i < 15; i++){    
+        passwordTwo.textContent += characters[Math.floor(Math.random() * characters.length)]
+    }
+}
